100703f39b83677c21a02a1ab5e53cd3423ffcb8
tovouk <burningremedy@gmail.com>
2018-08-14 19:00:23 -0500

    Finish Exercise

--- 3.08-Exercise-AddAFlavor-SpecificActivity/app/src/main/java/com/udacity/gradle/flavorspecificactivity/MainActivityFragment.java
+++ 3.08-Exercise-AddAFlavor-SpecificActivity/app/src/main/java/com/udacity/gradle/flavorspecificactivity/MainActivityFragment.java
@@ -1,6 +1,7 @@
 package com.udacity.gradle.flavorspecificactivity;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,7 +17,7 @@ public class MainActivityFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_main, container, false);
     }
